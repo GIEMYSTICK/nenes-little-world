08f9ded788e72bd4ce1ed6ebf07989adce718817
GIEMYSTICK <161915358+GIEMYSTICK@users.noreply.github.com>
2026-08-23 19:13:57 +0700

    Update nene.ts

--- data/nene.ts
+++ data/nene.ts
@@ -1,2 +1,2 @@
 export const NENE_BIRTH_DATE = { year: 2026, month: 7, day: 9 };
-export const photos = [];
+export const photos: {src:string;alt:string;caption:string}[] = [{src:'/fva.png',alt:'เนเน่',caption:'ความทรงจำของเนเน่'}];
