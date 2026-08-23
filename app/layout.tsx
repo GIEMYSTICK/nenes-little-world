e736ad3c2cd30c5e53c862c37cd960428f95e937
GIEMYSTICK <161915358+GIEMYSTICK@users.noreply.github.com>
2026-08-23 19:34:40 +0700

    Handle empty Vercel site URL

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ const mali = Mali({
 });
 
 const siteUrl = process.env.NEXT_PUBLIC_SITE_URL
-  ?? (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000");
+  || (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000");
 
 export const metadata: Metadata = {
   metadataBase: new URL(siteUrl),
